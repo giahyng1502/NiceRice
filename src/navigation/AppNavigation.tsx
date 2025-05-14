b956d211d1786c4c7b688dfe3411148d173bbc21
hungcy <hungcy1502@gmail.com>
2025-05-14 18:27:17 +0700

    feat(bottom-navigation): implement custom bottom navigation

--- src/navigation/AppNavigation.tsx
+++ src/navigation/AppNavigation.tsx
@@ -2,16 +2,18 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import IntroduceScreen from "../screens/introduct/introduce_screen";
+import BottomNavigation from "./BottomNavigation";
 const Stack = createNativeStackNavigator();
 const AppNavigation : React.FC = () => {
     return (
         <NavigationContainer>
             <Stack.Navigator
-                initialRouteName="Introduce"
+                initialRouteName="Main"
                 screenOptions={{
                     headerShown: false,
                 }}>
                 <Stack.Screen name="Introduce" component={IntroduceScreen} />
+                <Stack.Screen name="Main" component={BottomNavigation} />
             </Stack.Navigator>
         </NavigationContainer>
     )
---
+++ src/navigation/BottomNavigation.tsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MessageScreen from '../screens/messages/MessageScreen';
+import GroupScreen from '../screens/groups/GroupScreen';
+import ProfileScreen from '../screens/profile/ProfileScreen';
+import MoreScreen from '../screens/more/MoreScreen';
+
+// Import icon SVG
+import IconMessage from '../assets/svgs/ic_message';
+import IconGroup from '../assets/svgs/ic_groups';
+import IconProfile from '../assets/svgs/ic_profile';
+import IconMore from '../assets/svgs/icon_more';
+import {height, width} from "../styles/globalStyles";
+import {useTheme} from "../hooks/useTheme";
+
+const Tab = createBottomTabNavigator();
+
+const BottomNavigation = () => {
+  const theme = useTheme();
+  return (
+      <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused }) => {
+              let IconComponent;
+
+              switch (route.name) {
+                case 'Messages':
+                  IconComponent = IconMessage;
+                  break;
+                case 'Group':
+                  IconComponent = IconGroup;
+                  break;
+                case 'Profile':
+                  IconComponent = IconProfile;
+                  break;
+                case 'More':
+                  IconComponent = IconMore;
+                  break;
+                default:
+                  return null;
+              }
+              return (
+                  <View style={[styles.iconWrapper, focused && styles.iconWrapperActive]}>
+                    <IconComponent />
+                      <Text style={{
+                          fontSize: 10,
+                          color : focused ? 'white' : theme.text3,
+                          marginTop : 8,
+                          fontWeight : '700'
+                      }}>{route.name}</Text>
+                  </View>
+              );
+            },
+            headerShown: false,
+            tabBarShowLabel: false, // nếu không muốn hiện tên dưới icon
+            tabBarStyle: {
+              height: height*0.1,
+              paddingBottom: 16,
+              paddingTop: 16,
+              backgroundColor : theme.background
+            },
+          })}
+      >
+        <Tab.Screen name="Messages" component={MessageScreen} />
+        <Tab.Screen name="Group" component={GroupScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+        <Tab.Screen name="More" component={MoreScreen} />
+      </Tab.Navigator>
+  )
+};
+
+const styles = StyleSheet.create({
+  iconWrapper: {
+    padding: 6,
+    width : width*0.2,
+    height : height*0.08,
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  iconWrapperActive: {
+    backgroundColor: '#40C4FF',
+  },
+});
+
+export default BottomNavigation;
